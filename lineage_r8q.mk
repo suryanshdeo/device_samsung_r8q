2152df38cc1a06f8a245dfd7fe21076db663e62e
skye-tachyon <sonicgames772@gmail.com>
2026-05-25 16:25:10 -0400

    r8q: set device brand/manufacturer to google
    * This nullifies the need to use lsposed modules to get Galaxy Wearable working on PixelOS
    
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- lineage_r8q.mk
+++ lineage_r8q.mk
@@ -32,9 +32,9 @@ TARGET_SCREEN_WIDTH := 1080
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_r8q
 PRODUCT_DEVICE := r8q
-PRODUCT_BRAND := samsung
+PRODUCT_BRAND := google
 PRODUCT_MODEL := SM-G780G
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := google
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
